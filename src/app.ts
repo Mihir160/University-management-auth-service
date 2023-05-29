87f54c2bc114f2caec038894eb5ddb2d171b7b7c
Mihir160 <mihir.roy266@gmail.com>
2023-05-29 09:54:58 +0600

    [Done] cors express mongoose

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+const port = 3000;
+import cors from "cors";
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working successfully");
+});
+
+export default app;
